255c739bc1aad8bdcde827350d987d7fcfdbd793
shylusbhagya <shylusbhagya@gmail.com>
2023-05-27 15:32:32 +0200

    Add files via upload

---
+++ Data science tools.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #My first program
+
+# ##Introduction
+# "Welcome to my notebook"
+
+# ##Data Science Languages
+# Python
+# R
+# SQL
+# Julia
+# Jawascript
+# 
+
+# ## Data Science Libraries
+# 
+# - Pandas
+# - NumPy
+# - Scikit-learn
+# - TensorFlow
+# - PyTorch
+# - Keras
+# - Matplotlib
+# - Seaborn
+# - Plotly
+# - Statsmodels
+
+# ## Data Science Tools
+# 
+# | Tool         | Description                                           |
+# |--------------|-------------------------------------------------------|
+# | Python       | General-purpose programming language                   |
+# | R            | Statistical programming language                       |
+# | Julia        | High-level, high-performance programming language      |
+# | SAS          | Statistical analysis software                          |
+# | MATLAB       | Numerical computing and programming environment        |
+# | SQL          | Language for managing and querying databases           |
+# | Pandas       | Data manipulation and analysis library for Python      |
+# | NumPy        | Numerical computing library for Python                 |
+# | Scikit-learn | Machine learning library for Python                    |
+# | TensorFlow   | Open-source machine learning framework                 |
+# | PyTorch      | Open-source deep learning framework                    |
+# | Keras        | Neural networks library for Python                     |
+# | Matplotlib   | Data visualization library for Python                  |
+# | Seaborn      | Statistical data visualization library for Python      |
+# | Plotly       | Interactive visualization library for Python           |
+# | Statsmodels  | Statistical models and tests library for Python        |
+
+# 
+
+# ## Arithmetic Expression Examples
+# 
+# 
+# 
+# 1. Addition: 
+#    - `2 + 3` evaluates to 5
+# 2. Subtraction:
+#    - `10 - 5` evaluates to 5
+# 
+
+# In[3]:
+
+
+2+3
+
+
+# In[4]:
+
+
+2*3
+
+
+# In[6]:
+
+
+# Convert Minutes to Hours
+
+def convert_minutes_to_hours(minutes):
+    """
+    This function converts minutes to hours.
+    """
+    hours = minutes / 60
+    return hours
+
+# Test the function
+minutes = 300
+hours = convert_minutes_to_hours(minutes)
+print("Hours:", hours)
+
+
+# The objectives of this project are as follows:
+# 
+# 1. Analyze the existing data to identify patterns and trends.
+# 2. Develop a predictive model based on the analysis.
+# 3. Evaluate the performance of the predictive model using appropriate metrics.
+# 4. Fine-tune the model to improve its accuracy and generalization.
+# 5. Document the findings, methodology, and recommendations.
+# 6. Present the results and recommendations to stakeholders.
+# 7. Incorporate feedback from stakeholders and make necessary revisions.
+# 8. Implement the finalized predictive model into production.
+# 9. Monitor the model's performance in real-world scenarios and make necessary adjustments.
+# 10. Continuously update and improve the model based on new data and insights.
+# 
+
+# ## Author
+# 
+# The content in this notebook was created by **Shylu**.
+# 
+
+# 
